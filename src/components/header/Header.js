509cbd4f57f91b10f805feb6e5ae502d2f59ccd3
Anthony Ng'ang'a <ngash.anto@gmail.com>
2020-12-15 17:57:54 +0300

    Placed Chuck norris image in the header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,9 @@
 import "./Header.css"
-import "./images/Norris.svg"
+import norris from "./images/Norris.svg"
 function Header() {
   return (
     <header>
-      <h1 className="red">This is the header</h1>
+      <img src={norris} alt="Chuck Norris Picture" className="norris"/>
     </header>
   );
 }
