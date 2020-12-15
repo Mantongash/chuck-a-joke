bffe47a31d1befcc7c7ba1db0f12be12f1e30ab7
Anthony Ng'ang'a <ngash.anto@gmail.com>
2020-12-15 18:01:22 +0300

    Inserted the sub-heading in the header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,6 +5,7 @@ function Header() {
     <header>
       <img src={norris} alt="Chuck Norris Picture" className="norris"/>
       <h1 className="heading">CHUCK a JOKE</h1>
+      <p>Filter Chuck Norris Jokes by category.</p>
     </header>
   );
 }
