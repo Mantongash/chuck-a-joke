8ab86906782c75a51ddca19e79b86b934a1cc425
Anthony Ng'ang'a <ngash.anto@gmail.com>
2020-12-15 18:40:40 +0300

    Changed the App component from a functional component to a class-based component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import React, {Component}  from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import TopQuote from './components/top-quote/TopQuote';
 import Categories from './components/categories/Categories';
-import Jokes from './components/jokes';
+import Jokes from './components/jokes/Jokes';
 
-function App() {
-  return (
-    <div>
-    <Header/>
-    <main>
-    <TopQuote/>
-    <div>
-    <Categories/>
-    <Jokes/>
-    </div>
-    </main>
-    </div>
-  );
+class App extends Component {
+  render(){
+
+    return (
+        <div>
+        <Header/>
+        <main>
+        <TopQuote/>
+        <div>
+        <Categories/>
+        <Jokes/>
+        </div>
+        </main>
+        </div>
+    );
+
+  }
 }
+
 
 export default App;
