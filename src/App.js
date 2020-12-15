047cbda05a3c11efdbf728def45fa40471e028ea
Anthony Ng'ang'a <ngash.anto@gmail.com>
2020-12-15 18:22:02 +0300

    Imported top quote component into the app component and implemented it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/header/Header'
+import TopQuote from './components/top-quote/TopQuote'
 
 function App() {
   return (
     <div>
     <Header/>
+    <main>
+    <TopQuote/>
+    </main>
     </div>
   );
 }
