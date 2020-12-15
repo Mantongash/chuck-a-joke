b4aec11644db02470a9d3388433e7996ae328f3b
Anthony Ng'ang'a <ngash.anto@gmail.com>
2020-12-15 18:02:14 +0300

    Added a className to the sub-heading

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <header>
       <img src={norris} alt="Chuck Norris Picture" className="norris"/>
       <h1 className="heading">CHUCK a JOKE</h1>
-      <p>Filter Chuck Norris Jokes by category.</p>
+      <p className="subheading">Filter Chuck Norris Jokes by category.</p>
     </header>
   );
 }
