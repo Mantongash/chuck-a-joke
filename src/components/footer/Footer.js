e913b9e0d11e1cd8cada3e7a5512a7ee4732d437
Anthony Ng'ang'a <ngash.anto@gmail.com>
2020-12-15 21:28:34 +0300

    Created Footer component function and exported it

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer(){
+  return (
+    <p>&copy; 2020 - Crafted with &hearts; by <a href="https://www.github.com/Mantongash">Anthony Ng'ang'a</a></p>
+  )
+}
+
+export default Footer;
