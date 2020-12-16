070e7cea4a9d8656a91a67aa45304fd3159702a4
Anthony Ng'ang'a <ngash.anto@gmail.com>
2020-12-16 06:24:34 +0300

    Encloded Category component with a div and gave it a className of categories

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -3,7 +3,11 @@ import Category from '../category/Category';
 
 function Categories({categories}){
   return categories.map((category, index)=>{
-    return  <Category key={index} category={category} className="category"/>
+    return  (
+      <div className="categories">
+    <Category key={index} category={category} className="category"/>
+    </div>
+    )
   });
 }
 
