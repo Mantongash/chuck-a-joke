7a4adc270fc46beaf949cf7d1eb1655110992b61
Anthony Ng'ang'a <ngash.anto@gmail.com>
2020-12-16 06:18:55 +0300

    Added a category className

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -3,7 +3,7 @@ import Category from '../category/Category';
 
 function Categories({categories}){
   return categories.map((category, index)=>{
-    return  <Category key={index} category={category}/>
+    return  <Category key={index} category={category} className="category"/>
   });
 }
 
